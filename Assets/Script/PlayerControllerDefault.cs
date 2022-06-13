b07a5bea3deb80c696d6e809cabf25913b5dede1
Daisuke Tsukada <dsgarage@gmail.com>
2022-06-13 10:11:09 +0900

    配布用パッケージ、Scene追加

--- Assets/Script/PlayerControllerDefault.cs
+++ Assets/Script/PlayerControllerDefault.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 /// <summary>
@@ -9,35 +7,35 @@ using UnityEngine;
 [RequireComponent(typeof(CharacterController))]
 public class PlayerControllerDefault : MonoBehaviour
 {
-    [SerializeField] float speed = 6.0F;       //歩行速度
-    [SerializeField] float jumpSpeed = 8.0F;   //ジャンプ力
-    [SerializeField] float gravity = 20.0F;    //重力の大きさ
+    [SerializeField] private float speed = 6.0F;       //歩行速度
+    [SerializeField] private float jumpSpeed = 8.0F;   //ジャンプ力
+    [SerializeField] private float gravity = 20.0F;    //重力の大きさ
 
-    private CharacterController controller;
-    private Vector3 moveDirection = Vector3.zero;
-    private float moveHorizon;
-    private float moveVertical;
+    private CharacterController _controller;
+    private Vector3 _moveDirection = Vector3.zero;
+    private float _moveHorizon;
+    private float _moveVertical;
     
 
     void Start () {
-        controller = GetComponent<CharacterController>();
+        _controller = GetComponent<CharacterController>();
     }
 
     void Update () {
 
-        moveHorizon = Input.GetAxis ("Horizontal");    //左右矢印キーの値(-1.0~1.0)
-        moveVertical = Input.GetAxis ("Vertical");      //上下矢印キーの値(-1.0~1.0)
+        _moveHorizon = Input.GetAxis ("Horizontal");    //左右矢印キーの値(-1.0~1.0)
+        _moveVertical = Input.GetAxis ("Vertical");      //上下矢印キーの値(-1.0~1.0)
 
-        if (controller.isGrounded) {
-            moveDirection = new Vector3 (moveHorizon, 0, moveVertical);
-            moveDirection = transform.TransformDirection(moveDirection);
-            moveDirection *= speed;
+        if (_controller.isGrounded) {
+            _moveDirection = new Vector3 (_moveHorizon, 0, _moveVertical);
+            _moveDirection = transform.TransformDirection(_moveDirection);
+            _moveDirection *= speed;
             if (Input.GetButton("Jump"))
-                moveDirection.y = jumpSpeed;
+                _moveDirection.y = jumpSpeed;
         }
         
-        moveDirection.y -= gravity * Time.deltaTime;
-        controller.Move(moveDirection * Time.deltaTime);
+        _moveDirection.y -= gravity * Time.deltaTime;
+        _controller.Move(_moveDirection * Time.deltaTime);
 
     }
 }
